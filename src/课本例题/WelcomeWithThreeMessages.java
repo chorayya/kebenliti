64b716671fcb9bfbc536bee470ce91bed2fe91ef
chorayya <473217413@qq.com>
2020-09-29 15:33:42 +0800

    1-2

---
+++ src/课本例题/WelcomeWithThreeMessages.java
@@ -0,0 +1,11 @@
+package ¿Î±¾ÀýÌâ;
+
+public class WelcomeWithThreeMessages {
+	public static void main(String[] args) {
+		System.out.println("Programming is fun!");
+		System.out.println("Fundamentals First");
+		System.out.println("Problem Driven");
+	}
+}
+
+
