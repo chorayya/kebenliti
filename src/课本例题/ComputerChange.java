4b9d9e8a87e624a3033b11289a4b7443130f1bd0
chorayya <473217413@qq.com>
2020-10-08 16:02:18 +0800

    2-10

---
+++ src/课本例题/ComputerChange.java
@@ -0,0 +1,42 @@
+package ¿Î±¾ÀýÌâ;
+import java.util.Scanner;
+public class ComputerChange {
+	public static void main(String[] args) {
+		//Create a Scanner
+		Scanner input = new Scanner(System.in);
+		
+		//Receive the amount
+		System.out.print(
+				"Enter an amount in double, for example 11.56: ");
+		double amount = input.nextDouble();
+		
+		int remainingAmount = (int)(amount * 100);
+		
+		//Find the number of one dollars
+		int numberOfOneDollars = remainingAmount / 100;
+		remainingAmount = remainingAmount % 100;
+		
+		//Find the number of quarters in the reamining amount
+		int numberOfQuarters = remainingAmount / 25;
+		remainingAmount = remainingAmount % 25;
+		
+		//Find the number of dimes in the reamining amount
+		int numberOfDimes = remainingAmount / 10;
+		remainingAmount = remainingAmount % 10;
+		
+		//Find the number of nickles in the reamining amount
+		int numberOfNickles = remainingAmount / 5;
+		remainingAmount = remainingAmount % 5;
+		
+		//Find the number of pennies in the reamining amount
+		int numberOfPennies = remainingAmount;
+		
+		//Display results
+		System.out.println("Your amount " + amount + " consists of");
+		System.out.println(" " + numberOfOneDollars + " dollars");
+		System.out.println(" " + numberOfQuarters + " quarters ");
+		System.out.println(" " + numberOfDimes + " dimes ");
+		System.out.println(" " + numberOfNickles + " nickles ");
+		System.out.println(" " + numberOfPennies + " pennies ");
+	}
+}
