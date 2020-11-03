362d9fc9e42ad21928d15a573c6f26bbcb07a193
chorayya <473217413@qq.com>
2020-11-03 16:31:34 +0800

    9.8

---
+++ src/第九章/CircleWithPrivateDataFields.java
@@ -0,0 +1,34 @@
+package µÚ¾ÅÕÂ;
+
+public class CircleWithPrivateDataFields {
+/** The radius of the circle*/
+	private double radius =1;
+	private static int numberOfObjects=0;
+	
+	/**Construct a circle with radius 1*/
+	public CircleWithPrivateDataFields() {
+		numberOfObjects++;
+	}
+	
+	/**Construct a circle with a specified radius*/
+	public CircleWithPrivateDataFields(double newRadius) {
+		radius=newRadius;
+		numberOfObjects++;
+	}
+	
+	/**Return radius*/
+	public double getRadius() {
+		return radius;
+	}
+	
+	/**Set a new radius*/
+	public void setRadius(double newRadius) {
+		radius=(newRadius>=0)?newRadius:0;
+	}
+	
+	/**Return the area of this circle*/
+	public double getArea() {
+		return radius*radius*Math.PI;
+	}
+	
+}
