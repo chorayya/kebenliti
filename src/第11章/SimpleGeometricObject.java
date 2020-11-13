ab0ebe35afd4bfa8099e02297470420b566f2000
chorayya <473217413@qq.com>
2020-11-13 21:00:10 +0800

    11-1

---
+++ src/第11章/SimpleGeometricObject.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package ตฺ11ีย;
+
+/**
+ * @author choray
+ *
+ */
+public class SimpleGeometricObject {
+	private String color="white";
+	private boolean filled;
+	private java.util.Date dateCreated;
+	
+	/** Construct a default geometric object*/
+	
+}
