8b9b9b743bcc03cab9597a3f6f1b16b7996e8ffe
chorayya <473217413@qq.com>
2020-11-17 16:00:09 +0800

    11-2

---
+++ src/第11章/CircleFromSimpleGeometricObject.java
@@ -0,0 +1,66 @@
+package ตฺ11ีย;
+
+public class CircleFromSimpleGeometricObject 
+extends SimpleGeometricObject{
+private double radius;
+public CircleFromSimpleGeometricObject(){
+}
+
+public CircleFromSimpleGeometricObject(double radius){
+	this.radius=radius;
+}
+
+public CircleFromSimpleGeometricObject(double radius,
+		String color,boolean filled){
+	this.radius=radius;
+	setColor(color);
+	setFilled(filled);
+}
+
+/**Return radius*/
+public double getRadius() {
+	return radius;
+}
+
+/**Set a new radius*/
+public void setRadius(double radius) {
+	this.radius=radius;
+}
+
+
+/**return area*/
+public double getArea() {
+	return radius*radius*Math.PI;
+}
+
+/**return diameter*/
+public double getDiameter() {
+	return 2*radius;
+}
+
+/**return perimeter*/
+public double getPerimeter() {
+	return 2*radius*Math.PI;
+}
+
+/**print the circle info*/
+public void printCircle() {
+	System.out.println("The circle is created "+getDateCreated()+
+			" and the radius is "+radius);
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
