bca1251cf461d108d11e5689db3134b39fb95ce3
chorayya <473217413@qq.com>
2020-11-17 16:17:16 +0800

    11-3

--- src/第11章/RectangleFromSimpleGeometricObject.java
+++ src/第11章/RectangleFromSimpleGeometricObject.java
@@ -1,5 +1,61 @@
 package ตฺ11ีย;
 
-public class RectangleFromSimpleGeometricObject {
+public class RectangleFromSimpleGeometricObject 
+	extends SimpleGeometricObject{
+	private double width;
+	private double height;
+	
+	public RectangleFromSimpleGeometricObject() {
+	}
+	
+	public RectangleFromSimpleGeometricObject(
+			double width,double height) {
+		this.width=width;
+		this.height=height;
+	}
 
+	public RectangleFromSimpleGeometricObject(
+			double width,double height,String color,boolean filled) {
+		this.width=width;
+		this.height=height;
+		setColor(color);
+		setFilled(filled);
+		
+	}
+	
+	/**return width*/
+	public double getWidth() {
+		return width;
+	}
+	
+	/**set a new width*/
+	public void setWidth(double width) {
+		this.width=width;
+	}
+	
+	/**return height*/
+	public double getHeight() {
+		return height;
+	}
+	
+	/**set a new height*/
+	public void setHeight(double Height) {
+		this.height=height;
+	}
+	
+	/**return area*/
+	public double getArea() {
+		return width*height;
+	}
+	
+	/**return perimeter*/
+	public double getPerimeter() {
+		return 2*(width+height);
+	}
+	
+	
+	
+	
+	
+	
 }
