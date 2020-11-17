3bffbccd5b0ef99ae117e301a76a8efab2e82902
chorayya <473217413@qq.com>
2020-11-17 16:00:17 +0800

    11-1

--- src/第11章/SimpleGeometricObject.java
+++ src/第11章/SimpleGeometricObject.java
@@ -13,5 +13,58 @@ public class SimpleGeometricObject {
 	private java.util.Date dateCreated;
 	
 	/** Construct a default geometric object*/
+	public SimpleGeometricObject() {
+		dateCreated= new java.util.Date();
+	}
+	
+	/**Construct a default geometric object with the specified color
+	 * and filled value*/
+	public SimpleGeometricObject(String color, boolean filled) {
+		dateCreated= new java.util.Date();
+		this.color=color;
+		this.filled=filled;
+		
+	}
+	
+	/** return color */
+	public String getColor() {
+		return color;
+	}
+	
+	/** Set a new color*/
+	public void setColor(String color) {
+		this.color=color;
+	}
+	
+	/** Return filled. Since filled is boolean,
+	 * its getter method is named isFilled
+	 */
+	public boolean isFilled() {
+		return filled;
+	}
+	
+	/**Set a new filled*/
+	public void setFilled(boolean filled) {
+		this.filled=filled;
+	}
+	
+	/**Get dataCreated*/
+	public java.util.Date getDateCreated(){
+		return dateCreated;
+	}
+	
+	/** Return a string representation of this object*/
+	public String toString() {
+		return "Created on "+dateCreated+"\ncolor: "+color+" and filled: "+filled;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
