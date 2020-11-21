68414578fd4ef49c05f1cfbd944b5e966c82b179
chorayya <473217413@qq.com>
2020-11-21 19:31:38 +0800

    11-5

---
+++ src/第11章/PolymorphismDemo.java
@@ -0,0 +1,17 @@
+package ตฺ11ีย;
+
+public class PolymorphismDemo {
+/**Main method*/
+	public static void main(String[] args) {
+		//display circle and rectangle properties
+		displayObject(new CircleFromSimpleGeometricObject(1,"red",false));
+		displayObject(new RectangleFromSimpleGeometricObject(1,1,"black",true));
+
+	}
+
+	/**display geometric object properties*/
+	public static void displayObject(SimpleGeometricObject object) {
+		System.out.println("Created on "+object.getDateCreated()+
+				".Color is "+object.getColor());
+	}
+}
