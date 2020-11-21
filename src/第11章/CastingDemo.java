e7011ab7936f2a854bb33fc24c16823a52e3a798
chorayya <473217413@qq.com>
2020-11-21 20:28:04 +0800

    11-7

---
+++ src/第11章/CastingDemo.java
@@ -0,0 +1,28 @@
+package ตฺ11ีย;
+
+public class CastingDemo {
+
+	public static void main(String[] args) {
+		// create and initialize two objects
+		Object object1= new CircleFromSimpleGeometricObject(1);
+		Object object2= new RectangleFromSimpleGeometricObject(1,1);
+	
+		//display circle and rectangle
+		displayObject(object1);
+		displayObject(object2);
+	}
+
+	/**a method for displaying an object*/
+	public static void displayObject(Object object) {
+		if(object instanceof CircleFromSimpleGeometricObject) {
+			System.out.println("The circle area is "+
+		((CircleFromSimpleGeometricObject)object).getArea());
+			System.out.println("The circle diameter is "+
+					((CircleFromSimpleGeometricObject)object).getDiameter());
+		}
+		else if(object instanceof RectangleFromSimpleGeometricObject) {
+			System.out.println("The rectangle area is "+
+					((RectangleFromSimpleGeometricObject)object).getArea());
+		}
+	}
+}
